61638324945327c64a60f1cc7eb970acc5a88139
Coru89 <corey.james.keller@gmail.com>
2016-02-06 16:50:38 -0700

    jquery getting closer

--- source/scripts/components/services.js
+++ source/scripts/components/services.js
@@ -1,6 +1,12 @@
-'use strict'
+'use strict';
 
-$( document ).ready(function() {
+$( document ).ready(function () {
+	$('.st-services__tab').on('click', function () {
+		$('.st-services__tab').removeClass('st-services__item--active');
+		$(this).closest('.st-services__tab').toggleClass('st-services__item--active');
+	});
 
-
+	$('.st-services__tab-main').on('click', function () {
+		$(this).next('.st-services__content-box').toggleClass('st-services__content-box--active');
+	});
 });
