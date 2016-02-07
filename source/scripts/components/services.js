50450315866b404650965bf72f070fe55c0e65dd
Coru89 <corey.james.keller@gmail.com>
2016-02-06 18:21:11 -0700

    woot. more JQueer progress.

--- source/scripts/components/services.js
+++ source/scripts/components/services.js
@@ -3,9 +3,10 @@
 $( document ).ready(function () {
 	$('.st-services__tab').on('click', function () {
 		$('.st-services__tab').removeClass('st-services__item--active');
-		$(this).closest('.st-services__tab').toggleClass('st-services__item--active');
+		$(this).addClass('st-services__item--active');
+		$('.st-services__hidden--md').addClass('st-services__hidden');
+		$(this).children('.st-services__hidden--md').removeClass('st-services__hidden');
 	});
-
 	$('.st-services__tab-main').on('click', function () {
 		$(this).next('.st-services__content-box').toggleClass('st-services__content-box--active');
 	});
