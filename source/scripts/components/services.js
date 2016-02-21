f8b7917a9a968be4de1bb68a17fa130bfabffd21
Coru89 <corey.james.keller@gmail.com>
2016-02-21 14:14:22 -0700

    cleaned up code as per last chat w/ andrew

--- source/scripts/components/services.js
+++ source/scripts/components/services.js
@@ -2,27 +2,14 @@
 
 $( document ).ready(function () {
 
-	//specific to business services
-	$('.st-services__tab-business').on('click', function () {
-		$(this).removeClass('st-services__item--active-business');
-		$(this).addClass('st-services__item--active-business');
-		$('.st-services__tab-business').removeClass('st-services__item--active');
-		$(this).addClass('st-services__item--active');
-		$('.st-services__hidden-business').addClass('st-services__hidden');
-		$(this).children('.st-services__hidden-business').removeClass('st-services__hidden');
-	});
-
-	//specific to residential services
-	$('.st-services__tab-residential').on('click', function () {
-		$(this).removeClass('st-services__item--active-residential');
-		$(this).addClass('st-services__item--active-residential');
-		$('.st-services__tab-residential').removeClass('st-services__item--active');
-		$(this).addClass('st-services__item--active');
-		$('.st-services__hidden-residential').addClass('st-services__hidden');
-		$(this).children('.st-services__hidden-residential').removeClass('st-services__hidden');
-	});
-
 	$('.st-services__tab').on('click', function () {
+		//add active class to menu item + remove active class from all other items in menu
+		$(this).closest('.st-services').find('.st-services__tab').removeClass('st-services__item--active');
+		$(this).addClass('st-services__item--active');
+
+		//add hidden class to menu item + remove hidden class from all other items in menu
+		$(this).closest('.st-services').find('.st-services__hidden--md').addClass('st-services__hidden');
+		$(this).children('.st-services__hidden--md').removeClass('st-services__hidden');
 
 		//get content from correct service and display it into the single column row on ipad
 		var content = $(this).children('.st-services__hidden--md').text();
